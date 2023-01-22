eb557139ef8ff2305a7c3f82332b6e16e4a64900
Zineb2a <zineb.alaoui663@gmail.com>
2023-01-22 04:57:54 -0500

    final

--- main.js
+++ main.js
@@ -105,5 +105,149 @@ function changeBackgroundColor(elementId, color) {
     
   }
 
+function generatePassword() {
+    let password = "";
+    const topLeftKeys = [
+      "q",
+      "w",
+      "e",
+      "r",
+      "t",
+      "y",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "~",
+      "!",
+      "@",
+      "#",
+      "$",
+      "%",
+      "Q",
+      "W",
+      "E",
+      "R",
+      "T",
+      "Y",
+    ];
+    const topRightKeys = [
+      "u",
+      "i",
+      "o",
+      "p",
+      "[",
+      "]",
+      "7",
+      "8",
+      "9",
+      "0",
+      "-",
+      "=",
+      "^",
+      "&",
+      "*",
+      "(",
+      ")",
+      "_",
+      "+",
+      "U",
+      "I",
+      "O",
+      "P",
+      "{",
+      "}",
+    ];
+    const bottomLeftKeys = [
+      "a",
+      "s",
+      "d",
+      "f",
+      "g",
+      "h",
+      "z",
+      "x",
+      "c",
+      "v",
+      "b",
+      "n",
+      "m",
+      ",",
+      ".",
+      "/",
+      ":",
+      ";",
+      "'",
+      "<",
+      ">",
+      "?",
+      "A",
+      "S",
+      "D",
+      "F",
+      "G",
+      "H",
+      "Z",
+      "X",
+      "C",
+      "V",
+      "B",
+      "N",
+      "M",
+    ];
+    const bottomRightKeys = [
+      "j",
+      "k",
+      "l",
+      ";",
+      "'",
+      "b",
+      "n",
+      "m",
+      ",",
+      ".",
+      "/",
+      ":",
+      ";",
+      "'",
+      "<",
+      ">",
+      "?",
+      "J",
+      "K",
+      "L",
+      "B",
+      "N",
+      "M",
+    ];
+
+    //create a password of random length from 16 to 24
+    const passwordLength = Math.floor(Math.random() * 8) + 16;
+
+    //combine the random keys into a password
+    //the password will be a random combination of keys from each quater of the keyboard
+    //this is to avoid obvious keypaths like "qwerty" or "asdfgh"
+    for (let i = 0; i < passwordLength; i++) {
+      const randomKey = Math.floor(Math.random() * 4) + 1;
+
+      if (randomKey === 1) {
+        password += topLeftKeys[Math.floor(Math.random() * topLeftKeys.length)];
+      } else if (randomKey === 2) {
+        password +=
+          topRightKeys[Math.floor(Math.random() * topRightKeys.length)];
+      } else if (randomKey === 3) {
+        password +=
+          bottomLeftKeys[Math.floor(Math.random() * bottomLeftKeys.length)];
+      } else if (randomKey === 4) {
+        password +=
+          bottomRightKeys[Math.floor(Math.random() * bottomRightKeys.length)];
+      }
+}
+
+document.getElementById("new-pass").innerHTML = password;
+
+}
 
 
